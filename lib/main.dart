6bb0db854484a0e534987eb6116d938bc134a151
Himanshu Sharma <contact@himanshusharma.tech>
2024-09-20 21:01:13 +0530

    Pre final changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_dart_admin_auth_sdk/firebase_dart_admin_auth_sdk.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  String serviceAccountContent = await rootBundle.loadString(
+      'assets/service_account.json'); //Add your own JSON service account and make sure it is ignored in your version control system
+
+  // Initialize Firebase with the service account content
+  await FirebaseApp.initializeAppWithServiceAccount(
+    serviceAccountContent: serviceAccountContent,
+    serviceAccountKeyFilePath: '',
+  );
   runApp(const MyApp());
 }
 
@@ -25,10 +36,42 @@ class UserManagementScreen extends StatefulWidget {
 }
 
 class _UserManagementScreenState extends State<UserManagementScreen> {
+  // Creating an instance of the SDK
+  FirebaseAuth? firebaseAuth;
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
   final _roleController = TextEditingController();
   String _status = "";
+
+  // Initializing the instance in the initState of the Widget
+  @override
+  void initState() {
+    firebaseAuth = FirebaseApp.firebaseAuth;
+    super.initState();
+  }
+
+  Future<void> registerNewUser() async {
+    try {
+      // Using the instance to create a user with email and password with returns [UserCredential] object
+      var userCredential = await firebaseAuth?.createUserWithEmailAndPassword(
+          _emailController.text, _passwordController.text);
+
+      // Checking whethe the credntial are null or not and later updating the same with the rol
+      if (userCredential != null) {
+        firebaseAuth?.updateUserInformation(userCredential.user.uid,
+            userCredential.user.idToken!, {'role': _roleController.text});
+      }
+
+      setState(() {
+        _status =
+            "User created successfully with role: ${_roleController.text}";
+      });
+    } catch (e) {
+      setState(() {
+        _status = "Failed to create user: $e";
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +98,8 @@ class _UserManagementScreenState extends State<UserManagementScreen> {
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {},
+              // Calling the method on submitting the above information
+              onPressed: registerNewUser,
               child: const Text('Create User'),
             ),
             const SizedBox(height: 20),
